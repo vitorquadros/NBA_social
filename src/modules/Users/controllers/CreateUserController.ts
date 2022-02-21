195e6ae0662670b4c2c99a5371e3547a00a308d7
Vitor Quadros <viitorquadros@hotmail.com>
2022-02-21 20:32:27 -0300

    feat: create CreateUserController

---
+++ src/modules/Users/controllers/CreateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { Controller } from '../../interfaces/Controller';
+import { CreaterUserUsecase } from '../usecases/CreateUserUsecase';
+
+export class CreateUserController implements Controller {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { username, email, password } = req.body;
+    const createUserUsecase = new CreaterUserUsecase();
+
+    try {
+      const user = await createUserUsecase.execute({
+        username,
+        email,
+        password
+      });
+
+      return res.status(201).json({ status: 'ok', data: user });
+    } catch (error) {
+      return res.status(400).json({ status: 'error', error: error.message });
+    }
+  }
+}
