438b50c5a0382a239dc7cbc524da588640e40374
Vitor Quadros <viitorquadros@hotmail.com>
2022-02-22 20:09:22 -0300

    feat: create UsersAddressRelation migration

---
+++ src/infra/database/migrations/1645570803177-CreateUserAddressRelation.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export class CreateUserAddressRelation1645570803177
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        columnNames: ['address_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'adresses',
+        onDelete: 'CASCADE'
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('users');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('address_id') !== -1
+    );
+    await queryRunner.dropForeignKey('users', foreignKey);
+  }
+}
