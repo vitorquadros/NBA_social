ee6e1563d892ad884b41eae47b1fa7da83041973
Vitor Quadros <viitorquadros@hotmail.com>
2022-02-22 17:56:32 -0300

    feat: add columns to User model

--- src/modules/Users/models/User.ts
+++ src/modules/Users/models/User.ts
@@ -1,5 +1,18 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
+
+enum UserRole {
+  USER = 'user',
+  VERIFIED = 'verified',
+  VERIFIED_PLAYER = 'verified_player',
+  ADMIN = 'admin'
+}
 
 @Entity('users')
 export class User {
@@ -10,13 +23,31 @@ export class User {
   username: string;
 
   @Column()
+  display_name: string;
+
+  @Column()
+  birthday: Date;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @Column()
+  nba_team: string;
+
+  @Column()
   email: string;
 
   @Column()
   password: string;
 
+  @Column({ default: 'profile_default.jpg' })
+  avatar: string;
+
   @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
