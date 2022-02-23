b4a5244b59f71ac52014b7e46866bbf21ca44a01
Vitor Quadros <viitorquadros@hotmail.com>
2022-02-23 17:32:10 -0300

    feat: create GetUsers route

--- src/infra/http/routes/usersRoutes.ts
+++ src/infra/http/routes/usersRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { CreateUserController } from '../../../modules/Users/controllers/CreateUserController';
+import { GetUsersController } from '../../../modules/Users/controllers/GetUsersController';
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const getUsersController = new GetUsersController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.get('/', getUsersController.handle);
