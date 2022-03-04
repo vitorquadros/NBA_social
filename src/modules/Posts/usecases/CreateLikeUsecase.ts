e4a8cf76f4b10d35035647fd4bbfe82b8aaa1b3c
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-03 21:12:25 -0300

    feat: create CreateLikeUsecase

---
+++ src/modules/Posts/usecases/CreateLikeUsecase.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from 'tsyringe';
+import { getRepository } from 'typeorm';
+import { User } from '../../Users/models/User';
+import { Like } from '../models/Like';
+import { Post } from '../models/Post';
+
+type CreateLikeRequest = {
+  userId: string;
+  postId: string;
+};
+
+@injectable()
+export class CreateLikeUsecase {
+  // constructor(
+  //   @inject('PostsRepository')
+  //   private postsReopsitory: IPostsRepository
+  // ) {}
+
+  async execute({ userId, postId }: CreateLikeRequest): Promise<Like> {
+    const repository = getRepository(Like);
+
+    const usersRepository = getRepository(User);
+    const postsRepository = getRepository(Post);
+
+    const user = await usersRepository.findOne(userId);
+    const post = await postsRepository.findOne(postId);
+
+    const like = repository.create({ user, post });
+
+    await repository.save(like);
+
+    return like;
+  }
+}
