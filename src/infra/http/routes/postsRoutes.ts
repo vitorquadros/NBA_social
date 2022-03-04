4b6251a7fbf3550949ebb1dfc50461d1a85546bd
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-03 22:04:04 -0300

    feat: create get posts route

--- src/infra/http/routes/postsRoutes.ts
+++ src/infra/http/routes/postsRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { CreateLikeController } from '../../../modules/Posts/controllers/CreateLikeController';
 import { CreatePostController } from '../../../modules/Posts/controllers/CreatePostController';
+import { GetPostsController } from '../../../modules/Posts/controllers/GetPostsController';
 import authMiddleware from '../middlewares/authMiddleware';
 
 export const postsRoutes = Router();
 
 const createPostController = new CreatePostController();
+const getPostsController = new GetPostsController();
 const createLikeController = new CreateLikeController();
 
 postsRoutes.post('/', authMiddleware, createPostController.handle);
+postsRoutes.get('/', getPostsController.handle);
 postsRoutes.post('/:postId/like', authMiddleware, createLikeController.handle);
