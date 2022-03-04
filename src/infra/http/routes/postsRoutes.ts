b7db7ef86e3723c2fc52dc5182d4da88c81a163e
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-03 21:12:41 -0300

    feat: create create like on a post route

--- src/infra/http/routes/postsRoutes.ts
+++ src/infra/http/routes/postsRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { CreateLikeController } from '../../../modules/Posts/controllers/CreateLikeController';
 import { CreatePostController } from '../../../modules/Posts/controllers/CreatePostController';
 import authMiddleware from '../middlewares/authMiddleware';
 
 export const postsRoutes = Router();
 
 const createPostController = new CreatePostController();
+const createLikeController = new CreateLikeController();
 
 postsRoutes.post('/', authMiddleware, createPostController.handle);
+postsRoutes.post('/:postId/like', authMiddleware, createLikeController.handle);
