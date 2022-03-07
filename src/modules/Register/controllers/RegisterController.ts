7fc52f5c95de2e41ae519da30464fbbfb2e72b76
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-07 16:15:24 -0300

    feat: create index method to get all users at RegisterController

--- src/modules/Register/controllers/RegisterController.ts
+++ src/modules/Register/controllers/RegisterController.ts
@@ -3,6 +3,20 @@ import { container } from 'tsyringe';
 import { RegisterUsecase } from '../usecases/RegisterUsecase';
 
 export class RegisterController {
+  async index(req: Request, res: Response): Promise<Response> {
+    const registerUsecase = container.resolve(RegisterUsecase);
+
+    try {
+      const users = await registerUsecase.getRegisters();
+
+      return res
+        .status(200)
+        .json({ status: 'ok', usersCount: users.length, data: users });
+    } catch (error) {
+      return res.status(400).json({ status: 'error', error: error.message });
+    }
+  }
+
   async store(req: Request, res: Response): Promise<Response> {
     const { email, redirectUrl } = req.body;
 
