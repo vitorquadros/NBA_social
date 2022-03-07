bb2732c9f5e713bc59eef0cfedb6d80402063cb2
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-07 14:14:33 -0300

    refactor: move email logic to usecase

--- src/modules/Register/controllers/RegisterController.ts
+++ src/modules/Register/controllers/RegisterController.ts
@@ -3,8 +3,7 @@ import { container } from 'tsyringe';
 import { getRepository } from 'typeorm';
 import { UserKey } from '../models/UserKey';
 import { RegisterUsecase } from '../usecases/RegisterUsecase';
-import { v4 as uuid } from 'uuid';
-import nodemailer from 'nodemailer';
+
 import { User } from '../../Users/models/User';
 
 export class RegisterController {
@@ -14,33 +13,7 @@ export class RegisterController {
     const registerUsecase = container.resolve(RegisterUsecase);
 
     try {
-      const user = await registerUsecase.register(email);
-
-      const keysRepository = getRepository(UserKey);
-      const key = uuid();
-      const userKey = keysRepository.create({ userId: user, key });
-      await keysRepository.save(userKey);
-
-      const link = `${redirectUrl.replace(/\/$/, '')}/${key}`;
-
-      const transport = nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        auth: {
-          user: '7c73252f3b2703',
-          pass: '161a2c0cea727d'
-        }
-      });
-
-      const message = {
-        from: 'noreply@test.com',
-        to: 'user@test.com',
-        subject: 'Finalize seu cadastro',
-        text: `Para finalizar seu cadastro acesse o link: ${link}`,
-        html: `<p>Para finalizar seu cadastro acesse o link:</p> <a>${link}</a>`
-      };
-
-      transport.sendMail(message, (e) => console.log(e.message));
+      await registerUsecase.register(email, redirectUrl);
 
       return res.status(200).send();
     } catch (error) {
--- src/modules/Register/usecases/RegisterUsecase.ts
+++ src/modules/Register/usecases/RegisterUsecase.ts
@@ -1,8 +1,11 @@
 import { inject, injectable } from 'tsyringe';
 import { getRepository } from 'typeorm';
 import { User } from '../../Users/models/User';
+import { v4 as uuid } from 'uuid';
+import nodemailer from 'nodemailer';
 // import { IAdressesRepository } from '../repositories/IAdressesRepository';
 import { IUsersRepository } from '../repositories/IUsersRepository';
+import { UserKey } from '../models/UserKey';
 
 @injectable()
 export class RegisterUsecase {
@@ -11,12 +14,38 @@ export class RegisterUsecase {
     private usersRepository: IUsersRepository // @inject('AdressesRepository') // private adressesRepository: IAdressesRepository
   ) {}
 
-  async register(email: string): Promise<User> {
+  async register(email: string, redirectUrl: string): Promise<User> {
     if (await this.usersRepository.findByEmail(email)) {
       throw new Error('Email already exists');
     }
 
     const user = await this.usersRepository.registerUser(email);
+
+    const keysRepository = getRepository(UserKey);
+    const key = uuid();
+    const userKey = keysRepository.create({ userId: user, key });
+    await keysRepository.save(userKey);
+
+    const link = `${redirectUrl.replace(/\/$/, '')}/${key}`;
+
+    const transport = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: '7c73252f3b2703',
+        pass: '161a2c0cea727d'
+      }
+    });
+
+    const message = {
+      from: 'noreply@test.com',
+      to: 'user@test.com',
+      subject: 'Finalize seu cadastro',
+      text: `Para finalizar seu cadastro acesse o link: ${link}`,
+      html: `<p>Para finalizar seu cadastro acesse o link:</p> <a>${link}</a>`
+    };
+
+    transport.sendMail(message, (e) => console.log(e.message));
 
     return user;
   }
