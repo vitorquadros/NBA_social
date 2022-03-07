ad3ac965a789a9f09087c827dfe6e861272b7b93
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-07 11:57:18 -0300

    feat: change routes to new registration module

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -6,6 +6,6 @@ import { postsRoutes } from './postsRoutes';
 
 export const router = Router();
 
-router.use('/users', usersRoutes);
+router.use('/register', usersRoutes);
 router.use('/auth', authRoutes);
 router.use('/posts', postsRoutes);
--- src/infra/http/routes/usersRoutes.ts
+++ src/infra/http/routes/usersRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { CreateUserController } from '../../../modules/Users/controllers/CreateUserController';
-import { GetUsersController } from '../../../modules/Users/controllers/GetUsersController';
-import authMiddleware from '../middlewares/authMiddleware';
+import { RegisterController } from '../../../modules/Register/controllers/RegisterController';
+// import { CreateUserController } from '../../../modules/Users/controllers/CreateUserController';
+// import { GetUsersController } from '../../../modules/Users/controllers/GetUsersController';
+// import authMiddleware from '../middlewares/authMiddleware';
 
 export const usersRoutes = Router();
 
-const createUserController = new CreateUserController();
-const getUsersController = new GetUsersController();
+// const createUserController = new CreateUserController();
+// const getUsersController = new GetUsersController();
+const registerController = new RegisterController();
 
-usersRoutes.post('/', createUserController.handle);
-usersRoutes.get('/', authMiddleware, getUsersController.handle);
+// usersRoutes.post('/', createUserController.handle);
+// usersRoutes.get('/', authMiddleware, getUsersController.handle);
+usersRoutes.post('/', registerController.store);
