96d53efa887a52de74260a35dfc5f6de9be3bafb
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-15 19:13:55 -0300

    feat (routes): create CreateComment route

--- api/src/infra/http/routes/postsRoutes.ts
+++ api/src/infra/http/routes/postsRoutes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 import { LikesController } from '../../../modules/Posts/controllers/LikesController';
-
 import authMiddleware from '../middlewares/authMiddleware';
 import { PostsController } from '../../../modules/Posts/controllers/PostsController';
+import { CommentsController } from '../../../modules/Posts/controllers/CommentsController';
 
 export const postsRoutes = Router();
 
 const postsController = new PostsController();
 const likesController = new LikesController();
+const commentsController = new CommentsController();
 
 postsRoutes.post('/', authMiddleware, postsController.store);
 postsRoutes.get('/', postsController.index);
 postsRoutes.post('/:postId/like', authMiddleware, likesController.store);
+
+// Comments
+postsRoutes.post('/:postId/comments', authMiddleware, commentsController.store);
