fd311f2ce8d2f7dbbaa6965bf8af397c12ceb280
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-16 18:22:03 -0300

    config: install and setup cors

--- api/src/infra/http/app.ts
+++ api/src/infra/http/app.ts
@@ -1,10 +1,22 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import '../../shared/container';
-import express, { Application } from 'express';
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  request,
+  Response
+} from 'express';
 import '../database';
 import { router } from './routes';
 
 export const app: Application = express();
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  app.use(cors);
+  next();
+});
 app.use(express.json());
 app.use(router);
