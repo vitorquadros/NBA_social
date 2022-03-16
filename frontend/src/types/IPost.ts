5161bd7d8f8f14ce809b2b8c3b19c16244cd3b3f
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-16 19:44:17 -0300

    feat: create Post interface

---
+++ frontend/src/types/IPost.ts
@@ -0,0 +1,27 @@
+import { User } from './IUser';
+
+type Comment = {
+  id: string;
+  text: string;
+  parentComment?: Comment;
+  replys?: Comment[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Like = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export interface Post {
+  id: string;
+  description: string;
+  image: string;
+  likes?: Like[];
+  comments?: Comment[];
+  user: User;
+  createdAt: string;
+  updatedAt: string;
+}
