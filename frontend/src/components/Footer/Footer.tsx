3dcef97a43ab39a7b1cb9362e8279ef4f14c8151
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-17 12:10:50 -0300

    feat: create Footer component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default function Footer() {
+  return <div>Footer</div>;
+}
+
+const Container = styled.footer`
+  width: 100%;
+  min-height: 8rem;
+
+  background-color: white;
+`;
