a85c4b6b31be5a394919ad46faf51e821a3a7616
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-21 15:14:12 -0300

    feat: create Search component on Header

---
+++ frontend/src/components/Header/Search.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export default function Search() {
+  return (
+    <Container>
+      <div className="search-box">
+        <span className="material-icons">search</span>
+        <input type="text" />
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.form`
+  width: 35rem;
+
+  margin: 0 auto;
+
+  div.search-box {
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #efefef;
+
+    width: 100%;
+
+    span {
+      margin: 0.5rem 0.6rem;
+      color: #8e8e8e;
+    }
+
+    input {
+      box-sizing: border-box;
+      width: 100%;
+      margin-right: 1rem;
+      border: none;
+      background-color: #efefef;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
