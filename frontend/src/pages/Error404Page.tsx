71c4977a56f92f6f21961d741dd07ac75396fe69
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-24 10:09:08 -0300

    style: 404 page

--- frontend/src/pages/Error404Page.tsx
+++ frontend/src/pages/Error404Page.tsx
@@ -1,3 +1,29 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 export default function Error404Page() {
-  return <h1>Erro! Página não encontrada!</h1>;
+  return (
+    <Container>
+      <h1>404</h1>
+      <h2>Página não encontrada!</h2>
+      <Link to="/">Voltar para a página inicial</Link>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  min-width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h1 {
+    font-size: 8rem;
+  }
+
+  h2 {
+    font-size: 4rem;
+  }
+`;
