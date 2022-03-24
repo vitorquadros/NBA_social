9229b8db8448d5870bf4e8ce4f0caf840f076b20
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-24 14:56:02 -0300

    feat: create Register page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import CompleteRegisterForm from '../components/Auth/Register/CompleteRegisterForm';
+
+export default function Register() {
+  return (
+    <Container>
+      <h1>Finalize seu cadastro</h1>
+      <CompleteRegisterForm />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #fafafa;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin-top: 3rem;
+    font-size: 1.8rem;
+  }
+`;
