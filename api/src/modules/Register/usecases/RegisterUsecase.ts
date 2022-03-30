c36ab24ef77707ef38275c9035bbd9964052f227
Vitor Quadros <viitorquadros@hotmail.com>
2022-03-30 15:39:35 -0300

    fix (RegisterUsecase): email link

--- api/src/modules/Register/usecases/RegisterUsecase.ts
+++ api/src/modules/Register/usecases/RegisterUsecase.ts
@@ -63,7 +63,7 @@ export class RegisterUsecase {
       to: 'user@test.com',
       subject: 'Finalize seu cadastro',
       text: `Para finalizar seu cadastro acesse o link: ${link}`,
-      html: `<p>Para finalizar seu cadastro acesse o link:</p> <a>${link}</a>`
+      html: `<p>Para finalizar seu cadastro <a href="${link}">clique aqui</a> ou acesse o link:</p> <a>${link}</a>`
     };
 
     transport.sendMail(message, (e) => console.log(e.message));
