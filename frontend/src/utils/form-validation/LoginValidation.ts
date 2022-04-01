50f8aef44f0c0b2b7c1de20dcd4f60290054fd2c
Vitor Quadros <viitorquadros@hotmail.com>
2022-04-01 16:07:15 -0300

    feat: create LoginForm validation

---
+++ frontend/src/utils/form-validation/LoginValidation.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+export const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email('O formato do email é inválido')
+      .required('Este campo é obrigatório'),
+    password: yup.string().required('Este campo é obrigatório')
+  })
+  .required();
