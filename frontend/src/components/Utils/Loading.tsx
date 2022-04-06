c81ea217cd693e6b93eb830ded76406516ed70b6
Vitor Quadros <viitorquadros@hotmail.com>
2022-04-06 18:34:16 -0300

    feat (Loading): add inline loading component

--- frontend/src/components/Utils/Loading.tsx
+++ frontend/src/components/Utils/Loading.tsx
@@ -1,12 +1,34 @@
 import styled from 'styled-components';
 
-export default function Loading() {
+export default function LoadingFull() {
   return (
     <Container>
-      <div className="loader"></div>
+      <Spinner></Spinner>
     </Container>
   );
 }
+
+export function Loading() {
+  return <Spinner></Spinner>;
+}
+
+const Spinner = styled.div`
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #e56503;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1.5s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 const Container = styled.div`
   width: 100vw;
@@ -15,21 +37,8 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
 
-  div.loader {
-    border: 16px solid #f3f3f3;
-    border-top: 16px solid #e56503;
-    border-radius: 50%;
+  ${Spinner} {
     width: 60px;
     height: 60px;
-    animation: spin 1.5s linear infinite;
-
-    @keyframes spin {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
-    }
   }
 `;
