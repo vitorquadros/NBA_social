305ad4fb7ee2f72650ae94385c04e53cb3b7ddd5
Vitor Quadros <viitorquadros@hotmail.com>
2022-04-11 12:16:17 -0300

    style (PostForm): change warning color and textarea outline

--- frontend/src/components/Post/CreatePost/PostForm.tsx
+++ frontend/src/components/Post/CreatePost/PostForm.tsx
@@ -14,8 +14,8 @@ export default function PostForm() {
           <h3>Agora, escreva uma descrição para sua publicação</h3>
           <textarea />
           <span className="warning">
-            <span className="material-icons">question_mark</span>A descrição é
-            opcional
+            <span className="material-icons">help_outline</span>
+            <p>A descrição é opcional</p>
           </span>
         </>
       )}
@@ -83,14 +83,15 @@ const Form = styled.form`
     width: 100%;
     max-width: 80%;
 
-    border: 1px solid rgba(229, 101, 3, 1);
+    border: 1px solid black;
     border-radius: 5px;
     padding: 0.5rem 0 0 0.5rem;
     margin-bottom: 1rem;
-  }
 
-  textarea:focus {
-    outline: none;
+    &:focus {
+      outline: none !important;
+      border-color: rgba(229, 101, 3, 1);
+    }
   }
 
   span.warning {
@@ -98,14 +99,17 @@ const Form = styled.form`
     justify-content: center;
     align-items: center;
     margin-bottom: 2rem;
-    font-size: 1.4rem;
+
+    p {
+      font-size: 1.4rem;
+    }
 
     span {
-      font-size: 2rem;
-      background-color: rgb(251, 189, 4);
+      font-size: 2.4rem;
+
       border-radius: 50%;
       padding: 2px 3px;
-      margin-right: 3px;
+      margin-right: 1px;
     }
   }
 
