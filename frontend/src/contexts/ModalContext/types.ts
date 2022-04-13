01cd817d0d71efc43d3788fdfd33d423334244a9
Vitor Quadros <viitorquadros@hotmail.com>
2022-04-13 19:46:47 -0300

    fix (ModalContext): PostModal types

--- frontend/src/contexts/ModalContext/types.ts
+++ frontend/src/contexts/ModalContext/types.ts
@@ -1,15 +1,12 @@
 export interface IContext {
   showCreatePostModal: boolean;
   setShowCreatePostModal: (state: boolean) => void;
-  // openCreatePostModal: () => void;
 
-  showPostModal: boolean;
-  setShowPostModal: (state: boolean) => void;
-  // openPostModal: () => void;
+  showPostModal: string;
+  setShowPostModal: (postId: string) => void;
 
   showAuthModal: boolean;
   setShowAuthModal: (state: boolean) => void;
-  // openAuthModal: () => void;
 }
 
 export interface IModalProvider {
